e40dd764b16b8444b172e599fc8b2516024d1cc7
Clinton Wong <Owweeowwee@gmail.com>
2010-10-01 23:03:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlowerShop::Application.config.secret_token = 'ca71b53cc007a9c6f31b7f8d0bc4cb1ec12843ecf832d6cd59818549112f19db84163676f2df618d8be6957f315dd5ac5d264f0afc6203faa86d94bb0f01f33a'
